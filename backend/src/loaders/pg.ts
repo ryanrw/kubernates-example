bfa1c108b339f01abd97d57ca0e7c50c709821f8
Ryan-Willpower <surasak.c.work@gmail.com>
2019-11-27 19:57:17 +0700

    :fire: add connection error exception

--- backend/src/loaders/pg.ts
+++ backend/src/loaders/pg.ts
@@ -14,9 +14,21 @@ export async function initializeDatabaseClient() {
     await database.end()
     logger.error(error.message)
     const isAuthenticateError = error.message.includes(`authentication`)
+    const isConnectionDrop = error.message.includes(
+      `Connection terminated unexpectedly`
+    )
 
     if (isAuthenticateError) {
       throw new ApolloError(error.message, "DatabaseConnectError")
     }
+
+    if (isConnectionDrop) {
+      throw new ApolloError(
+        "Connection to database had been terminate. This approximately mean database is not running",
+        "DatabaseConnectError"
+      )
+    }
+
+    throw new ApolloError(error.message)
   }
 }

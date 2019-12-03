b32b2a92322a4aed6156c052d13fed0b78253b81
Ryan-Willpower <surasak.c.work@gmail.com>
2019-12-03 23:12:43 +0700

    feat: add jwt typedef

--- backend/src/graphql/typedefs/index.ts
+++ backend/src/graphql/typedefs/index.ts
@@ -2,7 +2,8 @@ import root from "./root"
 import status from "./status"
 import register from "./register"
 import login from "./login"
+import jwt from "./jwt"
 
-const typeDefs = [root, status, register, login]
+const typeDefs = [root, status, register, login, jwt]
 
 export default typeDefs
--- backend/src/graphql/typedefs/login.ts
+++ backend/src/graphql/typedefs/login.ts
@@ -2,6 +2,6 @@ import { gql } from "apollo-server"
 
 export default gql`
   extend type Mutation {
-    login(username: String, password: String): Status
+    login(username: String, password: String): JWT
   }
 `
